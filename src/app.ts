02530487a6d5b1ff8b2982b7f9f9bd9d27da1167
vini <velkhaliliborsato@gmail.com>
2024-08-21 03:36:20 -0300

    feat: initialize express server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
-import fs from 'fs'
-import { hey } from './helper.js'
-const ok = "ok"
-console.log(ok)
+import express, { Express } from "express";
+import { pool } from "./db/connectToDB.js";
+
+const port = 4000
+const app = express()
+app.listen(port,()=>{
+    console.log(`server is listening at port ${port}`)
+})
+
+app.get('/',(req,res)=>{
+    res.send(200)
+})
